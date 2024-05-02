02c59559d1fa22783dd9104bcbec4acfcb9840df
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-05-02 12:02:21 +0600

    add contracts page

---
+++ src/pages/dashboard/Contracts/ContractsTable.tsx
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import { Button, Card, Table } from "react-bootstrap";
+import Feather from "feather-icons-react"
+
+import classNames from "classnames";
+import { useAsyncDebounce } from "react-table";
+import Pagination from "../../../components/Pagination";
+import { records as data, } from "./data";
+import CustomeTable from "../../../components/CustomTable";
+
+const ContractsTable = (props: any) => {
+    interface TableRecords {
+        id: number;
+        firstName: string;
+        lastName: string;
+        userName: string;
+        sizePerPageList?: {
+            type: String;
+            value: number;
+        }[];
+    }
+
+    // dummy records
+    const records: TableRecords[] = [
+        { id: 1, firstName: "Mark", lastName: "Otto", userName: "@mdo" },
+        { id: 2, firstName: "Jacob", lastName: "Thornton", userName: "@fat" },
+        { id: 3, firstName: "Larry", lastName: "the Bird", userName: "@twitter" },
+        { id: 3, firstName: "Larry", lastName: "the Bird", userName: "@twitter" },
+        { id: 3, firstName: "Larry", lastName: "the Bird", userName: "@twitter" },
+        { id: 3, firstName: "Larry", lastName: "the Bird", userName: "@twitter" },
+        { id: 3, firstName: "Larry", lastName: "the Bird", userName: "@twitter" },
+    ];
+
+    const columns = [
+        {
+          Header: "#",
+          accessor: "#",
+          sort: true,
+        },
+        {
+          Header: "Title",
+          accessor: "title",
+          sort: true,
+        },
+        {
+          Header: "Discount",
+          accessor: "discount",
+          sort: false,
+        },
+        {
+          Header: "Action",
+          accessor: "action",
+          sort: true,
+        },
+      ];
+      
+      const sizePerPageList = [
+        {
+          text: "8",
+          value: 8,
+        },
+        {
+          text: "15",
+          value: 15,
+        },
+        {
+          text: "25",
+          value: 25,
+        },
+        {
+          text: "All",
+          value: data.length,
+        },
+      ];
+    return (
+        <>
+            <Card className="px-3 border-0">
+                <div className="">
+                    {/* <Table className="mb-0 border" striped>
+                        <thead className="bg-primary text-white">
+                            <tr>
+                                <th className="border-1" style={{ width: "0px" }}>#</th>
+                                <th className="border-1" style={{ width: "45%" }} scope="col">Title</th>
+                                <th className="border-1" style={{ width: "45%" }} scope="col">Discount</th>
+                                <th className="border-1" style={{ width: "10%" }} scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className="border-1">
+                            {(records || []).map((record, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <th scope="row" className="border-1">{record.id}</th>
+                                        <td className="border-1">{record.firstName}</td>
+                                        <td className="border-1">{record.lastName}</td>
+                                        <td className="border-1 d-flex align-items-center justify-content-center gap-2">
+
+                                            <>
+                                                <Button variant="primary" className="btn-sm d-flex align-items-center justify-content-center " style={{ width: "30px", height: "30px", borderRadius: "100%" }}>
+                                                    <i className="uil uil-edit" style={{ fontSize: "14px" }}></i>
+                                                </Button>
+                                                <Button variant="danger" className="btn-sm d-flex align-items-center justify-content-center" style={{ width: "30px", height: "30px", borderRadius: "100%" }}>
+                                                    <i className="uil uil-trash-alt" style={{ fontSize: "14px" }}></i>
+
+                                                </Button>
+                                            </>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table> */}
+                    {/* <CustomeTable
+                        columns={columns}
+                        data={data}
+                        pageSize={8}
+                        sizePerPageList={sizePerPageList}
+                        isSortable={true}
+                        pagination={true}
+                    /> */}
+                </div>
+            </Card>
+        </>
+    );
+};
+
+export default ContractsTable;
+
+
+
+// Define a default UI for filtering
+export const GlobalFilter = ({
+    setGlobalFilter,
+    searchBoxClass,
+}: any) => {
+    const [value, setValue] = useState<any>();
+    const onChange = useAsyncDebounce((value) => {
+        setGlobalFilter(value || undefined);
+    }, 200);
+
+    return (
+        <div className={classNames(searchBoxClass)}>
+            <span className="d-flex align-items-center">
+                Search :{" "}
+                <input
+                    type="search"
+                    value={value || ""}
+                    onChange={(e: any) => {
+                        setValue(e.target.value);
+                        onChange(e.target.value);
+                    }}
+                    placeholder={` `}
+                    className="form-control w-auto ms-1"
+                />
+            </span>
+        </div>
+    );
+};
---
+++ src/pages/dashboard/Contracts/index.tsx
@@ -0,0 +1,133 @@
+"use"
+import React, { useState } from 'react';
+
+
+// components
+import { Button, Card, Col, Form, Row, } from "react-bootstrap";
+import PageTitle from '../../../components/PageTitle';
+
+
+
+import ContractsTable, { GlobalFilter } from './ContractsTable';
+
+const columns = [
+    {
+        Header: "ID",
+        accessor: "id",
+        sort: true,
+    },
+    {
+        Header: "Name",
+        accessor: "name",
+        sort: true,
+    },
+    {
+        Header: "Phone Number",
+        accessor: "phone",
+        sort: false,
+    },
+    {
+        Header: "Age",
+        accessor: "age",
+        sort: true,
+    },
+    {
+        Header: "Company",
+        accessor: "company",
+        sort: false,
+    },
+];
+
+const sizePerPageList = [
+    {
+        text: "5",
+        value: 5,
+    },
+    {
+        text: "10",
+        value: 10,
+    },
+    {
+        text: "25",
+        value: 25,
+    },
+    {
+        text: "All",
+        value: 10,
+    },
+];
+
+
+
+const ContractsDashboard = () => {
+
+    const [newTaskDetails, setNewTaskDetails] = useState<any>(null);
+    // add task 
+    const newTask = (status: string, queue: string) => {
+        setNewTaskDetails({
+            dueDate: new Date(),
+            //   userAvatar: [defaultAvatar],
+            status: status,
+            queue: queue,
+        });
+        // setNewTaskModal(true);
+    };
+
+    return (
+        <>
+
+            <PageTitle
+                breadCrumbItems={[
+                    { label: "Dashboard", path: "/dashboard/Contracts" },
+                    { label: "Contracts", path: "/dashboard/contracts", active: true },
+                ]}
+                title={"Contracts"}
+            />
+            <Row>
+                <Col xs={12}>
+                    <Card>
+                        <Card.Body>
+                            <div className="float-sm-start">
+                                <label className="form-check-label">
+                                    Contracts Table
+                                </label>
+                            </div>
+                            <div className="float-sm-end">
+                                <Col className="text-sm-end mt-sm-0 mt-2">
+                                    <Button onClick={() => newTask("Pending", "todoTasks")}>
+                                        <i className="uil-plus me-1"></i>Add New
+                                    </Button>
+                                </Col>
+                            </div>
+                        </Card.Body>
+                        <hr />
+                        <>
+                            <div className="d-flex justify-content-between align-items-center px-3 pb-3">
+                                <div className="d-flex justify-content-start align-items-center gap-1">
+                                    Show <Form.Control className='w-25' type="number" placeholder="10" required /> records
+                                </div>
+                                <div className=''>
+                                    <p className="text-center text-muted pt-2 mb-0">
+                                        No Tasks
+                                    </p>
+                                </div>
+                                <div className="d-flex justify-content-center align-items-center">
+                                 {/* <GlobalFilter setGlobalFilter={5} searchBoxClass="suhil" /> */}
+                                </div>
+                            </div>
+                            {/* <ContractsTable /> */}
+                        </>
+
+
+                    </Card>
+
+                </Col>
+            </Row>
+
+
+
+        </>
+    );
+};
+
+export default ContractsDashboard;
